e9ca8612c640c0ae707c22e9f9a0c5affbe16f3d
vloup <vincent.loup@epfl.ch>
2015-02-27 13:40:48 +0100

    starting to complete Quad.h (should be okay), main.cpp (needs to work) and fix all kind of awful indentation...

--- hw2p3_planets/main.cpp
+++ hw2p3_planets/main.cpp
@@ -1,30 +1,39 @@
 #include "icg_common.h"
 #include "Quad.h"
 
-    
+
 Quad sun;
 Quad earth;
 Quad moon;
 
-void init(){
-    glClearColor(/*white*/ 1.0f,1.0f,1.0f, /*solid*/1.0 );
-    /// {stuff}.init(...);
+void init()
+{
+	glClearColor(/*white*/ 1.0f,1.0f,1.0f, /*solid*/1.0 );
+	/// {stuff}.init(...);
+	sun.init("sun.tga");
+	earth.init("earth.tga");
+	moon.init("moon.tga");
 }
 
-void display(){
-    glClear(GL_COLOR_BUFFER_BIT);
-    float time_s = glfwGetTime();
+void display()
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	float time_s = glfwGetTime();
 
-    /// {stuff}.draw(...);
+	/// {stuff}.draw(...);
 }
 
-int main(int, char**){
-    glfwInitWindowSize(800, 800);
-    glfwCreateWindow("planets");
-    glfwDisplayFunc(display);
-    init();
-    glfwMainLoop();
+int main(int, char**)
+{
+	glfwInitWindowSize(800, 800);
+	glfwCreateWindow("planets");
+	glfwDisplayFunc(display);
+	init();
+	glfwMainLoop();
 
-    //// {stuff}.cleanup()
-    return EXIT_SUCCESS;
+	//// {stuff}.cleanup()
+	sun.cleanup();
+	earth.cleanup();
+	moon.cleanup();
+	return EXIT_SUCCESS;
 }
