2167283bb1ddae328808fb08b3c89c81e9331e58
vloup <vincent.loup@epfl.ch>
2015-03-03 11:36:02 +0100

    remove on magic number in hw2p2 fshader

--- hw2p2_checkerboard/quad_fshader.glsl
+++ hw2p2_checkerboard/quad_fshader.glsl
@@ -3,9 +3,11 @@ uniform sampler1D colormap;
 in vec2 uv;
 out vec3 color;
 
+#define M_PI 3.14159265358979323846
+
 void main()
 {
-    float value = ((sin(uv.x * 31.5) * sin(uv.y * 31.5)) + 1) / 2;
+    float value = ((sin(uv.x * 10 * M_PI) * sin(uv.y * 10 * M_PI)) + 1) / 2;
     color = texture(colormap, value).rgb;
 
 }
