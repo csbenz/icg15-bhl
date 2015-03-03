ecbf5c38d33c28ea0e12e9421142323b37f17c3e
vloup <vincent.loup@epfl.ch>
2015-03-03 13:28:58 +0100

    refactor hw2p2

--- hw2p2_checkerboard/Quad.h
+++ hw2p2_checkerboard/Quad.h
@@ -3,97 +3,95 @@
 
 class Quad{
 private:
-    GLuint _vao; ///< vertex array object
-    GLuint _pid; ///< GLSL shader program ID 
-    GLuint _vbo; ///< memory buffer
-    GLuint _tex; ///< Texture ID
+	GLuint _vao; ///< vertex array object
+	GLuint _pid; ///< GLSL shader program ID
+	GLuint _vbo; ///< memory buffer
+	GLuint _tex; ///< Texture ID
 public:
-    void init(){
-        ///--- Compile the shaders
-        _pid = opengp::load_shaders("quad_vshader.glsl", "quad_fshader.glsl");
-        if(!_pid) exit(EXIT_FAILURE);       
-        glUseProgram(_pid);
-        
-        ///--- Vertex one vertex Array
-        glGenVertexArrays(1, &_vao);
-        glBindVertexArray(_vao);
-     
-        ///--- Vertex coordinates
-        {
-            const GLfloat vpoint[] = { /*V1*/ -1.0f, -1.0f, 0.0f, 
-                                       /*V2*/ +1.0f, -1.0f, 0.0f, 
-                                       /*V3*/ -1.0f, +1.0f, 0.0f,
-                                       /*V4*/ +1.0f, +1.0f, 0.0f };        
-            ///--- Buffer
-            glGenBuffers(1, &_vbo);
-            glBindBuffer(GL_ARRAY_BUFFER, _vbo);
-            glBufferData(GL_ARRAY_BUFFER, sizeof(vpoint), vpoint, GL_STATIC_DRAW);
-        
-            ///--- Attribute
-            GLuint vpoint_id = glGetAttribLocation(_pid, "vpoint");
-            glEnableVertexAttribArray(vpoint_id);
-            glVertexAttribPointer(vpoint_id, 3, GL_FLOAT, DONT_NORMALIZE, ZERO_STRIDE, ZERO_BUFFER_OFFSET);
-        }
-        
-        ///--- Texture coordinates
-        {
-            const GLfloat vtexcoord[] = { /*V1*/ 0.0f, 0.0f, 
-                                          /*V2*/ 1.0f, 0.0f, 
-                                          /*V3*/ 0.0f, 1.0f,
-                                          /*V4*/ 1.0f, 1.0f}; 
-            
-            ///--- Buffer
-            glGenBuffers(1, &_vbo);
-            glBindBuffer(GL_ARRAY_BUFFER, _vbo);
-            glBufferData(GL_ARRAY_BUFFER, sizeof(vtexcoord), vtexcoord, GL_STATIC_DRAW);
-        
-            ///--- Attribute
-            GLuint vtexcoord_id = glGetAttribLocation(_pid, "vtexcoord");
-            glEnableVertexAttribArray(vtexcoord_id);
-            glVertexAttribPointer(vtexcoord_id, 2, GL_FLOAT, DONT_NORMALIZE, ZERO_STRIDE, ZERO_BUFFER_OFFSET);
-        }
-        
-        ///
-        /// TODO: create the 1D texture (see slides)
-        /// 
-        {
-            const int sz = 3;
-            GLfloat tex[3 * sz] = {/*red*/ 1.0, 0.0, 0.0,
-                                /*yellow*/ 1.0, 1.0, 0.0,
-                                 /*green*/ 0.0, 1.0, 0.0};
-            glGenTextures(1, &_tex);
-            glBindTexture(GL_TEXTURE_1D, _tex);
-            glTexImage1D(GL_TEXTURE_1D, 0, GL_RGB, sz, 0, GL_RGB, GL_FLOAT, tex);
-            glTexParameteri(GL_TEXTURE_1D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-            glTexParameteri(GL_TEXTURE_1D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-            glTexParameteri(GL_TEXTURE_1D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-            GLuint tex_id = glGetUniformLocation(_pid, "colormap");
-            glUniform1i(tex_id, 0 /*GL_TEXTURE0*/);
-        }
-        
-        ///--- to avoid the current object being polluted
-        glBindVertexArray(0);
-        glUseProgram(0);
-    }
-       
-    void cleanup(){
-        /// TODO quad cleanup 
-	glDeleteBuffers(1, &_vbo);
-	glDeleteProgram(_pid);
-	glDeleteVertexArrays(1, &_vao);
-	glDeleteTextures(1, &_tex);
-    }
-    
-    void draw(float time /*seconds*/){
-        glUseProgram(_pid);
-        glBindVertexArray(_vao);        
-            ///--- Bind textures
-            glActiveTexture(GL_TEXTURE0);
-            glBindTexture(GL_TEXTURE_2D, _tex);
-           
-            ///--- Draw
-            glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
-        glBindVertexArray(0);        
-        glUseProgram(0);
-    }
+	void init(){
+		///--- Compile the shaders
+		_pid = opengp::load_shaders("quad_vshader.glsl", "quad_fshader.glsl");
+		if(!_pid) exit(EXIT_FAILURE);
+		glUseProgram(_pid);
+
+		///--- Vertex one vertex Array
+		glGenVertexArrays(1, &_vao);
+		glBindVertexArray(_vao);
+
+		///--- Vertex coordinates
+		{
+			const GLfloat vpoint[] = { /*V1*/ -1.0f, -1.0f, 0.0f,
+						   /*V2*/ +1.0f, -1.0f, 0.0f,
+						   /*V3*/ -1.0f, +1.0f, 0.0f,
+						   /*V4*/ +1.0f, +1.0f, 0.0f};
+			///--- Buffer
+			glGenBuffers(1, &_vbo);
+			glBindBuffer(GL_ARRAY_BUFFER, _vbo);
+			glBufferData(GL_ARRAY_BUFFER, sizeof(vpoint), vpoint, GL_STATIC_DRAW);
+
+			///--- Attribute
+			GLuint vpoint_id = glGetAttribLocation(_pid, "vpoint");
+			glEnableVertexAttribArray(vpoint_id);
+			glVertexAttribPointer(vpoint_id, 3, GL_FLOAT, DONT_NORMALIZE, ZERO_STRIDE, ZERO_BUFFER_OFFSET);
+		}
+
+		///--- Texture coordinates
+		{
+			const GLfloat vtexcoord[] = { /*V1*/ 0.0f, 0.0f,
+						      /*V2*/ 1.0f, 0.0f,
+						      /*V3*/ 0.0f, 1.0f,
+						      /*V4*/ 1.0f, 1.0f};
+
+			///--- Buffer
+			glGenBuffers(1, &_vbo);
+			glBindBuffer(GL_ARRAY_BUFFER, _vbo);
+			glBufferData(GL_ARRAY_BUFFER, sizeof(vtexcoord), vtexcoord, GL_STATIC_DRAW);
+
+			///--- Attribute
+			GLuint vtexcoord_id = glGetAttribLocation(_pid, "vtexcoord");
+			glEnableVertexAttribArray(vtexcoord_id);
+			glVertexAttribPointer(vtexcoord_id, 2, GL_FLOAT, DONT_NORMALIZE, ZERO_STRIDE, ZERO_BUFFER_OFFSET);
+		}
+
+		///--- create the 1D texture (see slides)
+		{
+			const int sz = 3;
+			GLfloat tex[3 * sz] = {/*red*/ 1.0, 0.0, 0.0,
+					    /*yellow*/ 1.0, 1.0, 0.0,
+					     /*green*/ 0.0, 1.0, 0.0};
+			glGenTextures(1, &_tex);
+			glBindTexture(GL_TEXTURE_1D, _tex);
+			glTexImage1D(GL_TEXTURE_1D, 0, GL_RGB, sz, 0, GL_RGB, GL_FLOAT, tex);
+			glTexParameteri(GL_TEXTURE_1D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+			glTexParameteri(GL_TEXTURE_1D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+			glTexParameteri(GL_TEXTURE_1D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+			GLuint tex_id = glGetUniformLocation(_pid, "colormap");
+			glUniform1i(tex_id, 0 /*GL_TEXTURE0*/);
+		}
+
+		///--- to avoid the current object being polluted
+		glBindVertexArray(0);
+		glUseProgram(0);
+	}
+
+	void cleanup(){
+		glDeleteBuffers(1, &_vbo);
+		glDeleteProgram(_pid);
+		glDeleteVertexArrays(1, &_vao);
+		glDeleteTextures(1, &_tex);
+	}
+
+	void draw(float time /*seconds*/){
+		glUseProgram(_pid);
+		glBindVertexArray(_vao);
+
+		///--- Bind textures
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, _tex);
+
+		///--- Draw
+		glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+		glBindVertexArray(0);
+		glUseProgram(0);
+	}
 };
