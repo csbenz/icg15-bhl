155ceac04409b130ecb1c597d0e56bb747b1788d
vloup <vincent.loup@epfl.ch>
2015-03-08 21:29:29 +0100

    grid and wave done

--- hw3_trackball/_grid/grid.h
+++ hw3_trackball/_grid/grid.h
@@ -3,112 +3,142 @@
 
 class Grid{
 protected:
-    GLuint _vao;          ///< vertex array object
-    GLuint _vbo_position; ///< memory buffer for positions
-    GLuint _vbo_index;    ///< memory buffer for indice
-    GLuint _pid;          ///< GLSL shader program ID
-    GLuint _tex;          ///< Texture ID
-    GLuint _num_indices;  ///< number of vertices to render
-    
+	GLuint _vao;          ///< vertex array object
+	GLuint _vbo_position; ///< memory buffer for positions
+	GLuint _vbo_index;    ///< memory buffer for indice
+	GLuint _pid;          ///< GLSL shader program ID
+	GLuint _tex;          ///< Texture ID
+	GLuint _num_indices;  ///< number of vertices to render
+
 public:    
-    void init(){
-        // Compile the shaders
-        _pid = opengp::load_shaders("grid_vshader.glsl", "grid_fshader.glsl");
-        if(!_pid) exit(EXIT_FAILURE);       
-        glUseProgram(_pid);
-        
-        // Vertex one vertex Array
-        glGenVertexArrays(1, &_vao);
-        glBindVertexArray(_vao);
-     
-        // Vertex coordinates and indices
-        {
-            std::vector<GLfloat> vertices;
-            std::vector<GLuint> indices;
-            // TODO 5: Make a triangle grid with dimension 100x100.
-            // Always two subsequent entries in 'vertices' form a 2D vertex position.
-            int grid_dim = 100;
+	void init(){
+		// Compile the shaders
+		_pid = opengp::load_shaders("grid_vshader.glsl", "grid_fshader.glsl");
+		if(!_pid) exit(EXIT_FAILURE);       
+		glUseProgram(_pid);
 
-            // The given code below are the vertices for a simple quad.
-            // Your grid should have the same dimension as that quad, i.e.,
-            // reach from [-1, -1] to [1, 1].
+		// Vertex one vertex Array
+		glGenVertexArrays(1, &_vao);
+		glBindVertexArray(_vao);
 
-            // Vertex position of the triangles.
-            vertices.push_back(-1.0f); vertices.push_back( 1.0f);
-            vertices.push_back( 1.0f); vertices.push_back( 1.0f);
-            vertices.push_back( 1.0f); vertices.push_back(-1.0f);
-            vertices.push_back(-1.0f); vertices.push_back(-1.0f);
+		// Vertex coordinates and indices
+		{
+			std::vector<GLfloat> vertices;
+			std::vector<GLuint> indices;
+			// TODO 5: Make a triangle grid with dimension 100x100.
+			// Always two subsequent entries in 'vertices' form a 2D vertex position.
+			int grid_dim = 100;
 
-            // And indices.
-            indices.push_back(0);
-            indices.push_back(1);
-            indices.push_back(3);
-            indices.push_back(2);
+			for (int height = 0; height <= grid_dim; ++height) {
+				for (int width = 0; width <= grid_dim; ++width) {
+					/* scaling width and height to fit on the canvas */
+					float x_vertex = (2.0 * ((float) width / (float) grid_dim)) - 1.0;
+					float y_vertex = (-2.0 * ((float) height / (float) grid_dim)) + 1.0;
 
-            _num_indices = indices.size();
+					/* push them as vertices */
+					vertices.push_back(x_vertex);
+					vertices.push_back(y_vertex);
+				}
+			}
 
-            // position buffer
-            glGenBuffers(1, &_vbo_position);
-            glBindBuffer(GL_ARRAY_BUFFER, _vbo_position);
-            glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(GLfloat), &vertices[0], GL_STATIC_DRAW);
+			/* forming triangles */
+			for (int height = 0; height < grid_dim; ++height) {
+				for (int width = 0; width < grid_dim; ++width) {
+					/* getting the indices */
+					int topLeft_vertex = (height * (grid_dim + 1)) + width;
+					int topRight_vertex = topLeft_vertex + 1;
+					int bottomLeft_vertex = topLeft_vertex + (grid_dim + 1); 
+					int bottomRight_vertex = bottomLeft_vertex + 1; 
 
-            // vertex indices
-            glGenBuffers(1, &_vbo_index);
-            glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, _vbo_index);
-            glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(GLuint), &indices[0], GL_STATIC_DRAW);
+					/* pushing */
+					indices.push_back(topLeft_vertex);
+					indices.push_back(topRight_vertex);
+					indices.push_back(bottomLeft_vertex);
+					indices.push_back(bottomRight_vertex);
+				}
+			}
 
-            // position shader attribute
-            GLuint loc_position = glGetAttribLocation(_pid, "position");
-            glEnableVertexAttribArray(loc_position);
-            glVertexAttribPointer(loc_position, 2, GL_FLOAT, DONT_NORMALIZE, ZERO_STRIDE, ZERO_BUFFER_OFFSET);
-        }
-        
-        // Load texture
-        glGenTextures(1, &_tex);
-        glBindTexture(GL_TEXTURE_2D, _tex);
-        glfwLoadTexture2D("grid_texture.tga", 0);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-                
-        // Texture uniforms
-        GLuint tex_id = glGetUniformLocation(_pid, "tex");
-        glUniform1i(tex_id, 0 /*GL_TEXTURE0*/);
-        
-        // to avoid the current object being polluted
-        glBindVertexArray(0);
-    }
-           
-    void cleanup(){
-        glDeleteBuffers(1, &_vbo_position);
-        glDeleteBuffers(1, &_vbo_index);
-        glDeleteVertexArrays(1, &_vao);
-        glDeleteProgram(_pid);
-        glDeleteTextures(1, &_tex);
-    }
-    
-    void draw(const mat4& model, const mat4& view, const mat4& projection, float time){
-        glUseProgram(_pid);
-        glBindVertexArray(_vao);
-        // Bind textures
-        glActiveTexture(GL_TEXTURE0);
-        glBindTexture(GL_TEXTURE_2D, _tex);
+#if 0
+			// The given code below are the vertices for a simple quad.
+			// Your grid should have the same dimension as that quad, i.e.,
+			// reach from [-1, -1] to [1, 1].
 
-        // Setup MVP
-        mat4 MVP = projection*view*model;
-        GLuint MVP_id = glGetUniformLocation(_pid, "mvp");
-        glUniformMatrix4fv(MVP_id, 1, GL_FALSE, MVP.data());
+			// Vertex position of the triangles.
+			vertices.push_back(-1.0f); vertices.push_back( 1.0f);
+			vertices.push_back( 1.0f); vertices.push_back( 1.0f);
+			vertices.push_back( 1.0f); vertices.push_back(-1.0f);
+			vertices.push_back(-1.0f); vertices.push_back(-1.0f);
 
-        // Pass the current time stamp to the shader.
-        glUniform1f(glGetUniformLocation(_pid, "time"), time);
+			// And indices.
+			indices.push_back(0);
+			indices.push_back(1);
+			indices.push_back(3);
+			indices.push_back(2);
+#endif
+			_num_indices = indices.size();
 
-        // Draw
-        // TODO 5: For debugging it can be helpful to draw only the wireframe.
-        // You can do that by uncommenting the next line.
-        //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-        // TODO 5: Depending on how you set up your vertex index buffer, you
-        // might have to change GL_TRIANGLE_STRIP to GL_TRIANGLES.
-        glDrawElements(GL_TRIANGLE_STRIP, _num_indices, GL_UNSIGNED_INT, 0);
-        glBindVertexArray(0);        
-        glUseProgram(0);
-    }
+			// position buffer
+			glGenBuffers(1, &_vbo_position);
+			glBindBuffer(GL_ARRAY_BUFFER, _vbo_position);
+			glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(GLfloat), &vertices[0], GL_STATIC_DRAW);
+
+			// vertex indices
+			glGenBuffers(1, &_vbo_index);
+			glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, _vbo_index);
+			glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(GLuint), &indices[0], GL_STATIC_DRAW);
+
+			// position shader attribute
+			GLuint loc_position = glGetAttribLocation(_pid, "position");
+			glEnableVertexAttribArray(loc_position);
+			glVertexAttribPointer(loc_position, 2, GL_FLOAT, DONT_NORMALIZE, ZERO_STRIDE, ZERO_BUFFER_OFFSET);
+		}
+
+		// Load texture
+		glGenTextures(1, &_tex);
+		glBindTexture(GL_TEXTURE_2D, _tex);
+		glfwLoadTexture2D("grid_texture.tga", 0);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+
+		// Texture uniforms
+		GLuint tex_id = glGetUniformLocation(_pid, "tex");
+		glUniform1i(tex_id, 0 /*GL_TEXTURE0*/);
+
+		// to avoid the current object being polluted
+		glBindVertexArray(0);
+	}
+
+	void cleanup(){
+		glDeleteBuffers(1, &_vbo_position);
+		glDeleteBuffers(1, &_vbo_index);
+		glDeleteVertexArrays(1, &_vao);
+		glDeleteProgram(_pid);
+		glDeleteTextures(1, &_tex);
+	}
+
+	void draw(const mat4& model, const mat4& view, const mat4& projection, float time){
+		glUseProgram(_pid);
+		glBindVertexArray(_vao);
+		// Bind textures
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, _tex);
+
+		// Setup MVP
+		mat4 MVP = projection*view*model;
+		GLuint MVP_id = glGetUniformLocation(_pid, "mvp");
+		glUniformMatrix4fv(MVP_id, 1, GL_FALSE, MVP.data());
+
+		// Pass the current time stamp to the shader.
+		glUniform1f(glGetUniformLocation(_pid, "time"), time);
+
+		// Draw
+		// TODO 5: For debugging it can be helpful to draw only the wireframe.
+		// You can do that by uncommenting the next line.
+		//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+		// TODO 5: Depending on how you set up your vertex index buffer, you
+		// might have to change GL_TRIANGLE_STRIP to GL_TRIANGLES.
+		glDrawElements(GL_TRIANGLE_STRIP, _num_indices, GL_UNSIGNED_INT, 0);
+		glBindVertexArray(0);        
+		glUseProgram(0);
+	}
 };
--- hw3_trackball/_grid/grid_vshader.glsl
+++ hw3_trackball/_grid/grid_vshader.glsl
@@ -7,6 +7,8 @@ uniform float time;
 in vec2 position;
 out vec2 uv;
 
+#define M_PI 3.141592653588
+
 void main() {
     uv = (position + vec2(1.0, 1.0)) * 0.5;
 
@@ -15,6 +17,7 @@ void main() {
     // TODO 6: Animate the height of the grid points as a sine function of the
     // 'time' and the position ('uv') within the grid.
     float height = 0.0;
+    height = 0.1 * sin(5 * M_PI * uv.y + 5 * time);
     vec3 pos_3d = vec3(position.x, height, -position.y);
 
     gl_Position = mvp * vec4(pos_3d, 1.0);
